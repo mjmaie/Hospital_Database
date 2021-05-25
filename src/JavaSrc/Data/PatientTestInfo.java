37caa6b151ce1b60a6a15baa5e81e119660aa698
mjmaie <61925367+mjmaie@users.noreply.github.com>
2021-05-24 22:39:03 -0400

    Update PatientTestInfo.java

--- src/JavaSrc/Data/PatientTestInfo.java
+++ src/JavaSrc/Data/PatientTestInfo.java
@@ -13,28 +13,7 @@ public class PatientTestInfo
 	{
 		id = testId;
 	}
-	
-//	public static PatientTestInfo loadPatient(int patientId) throws NoSuchPatientException
-//	{
-//		try
-//		{
-////			ResultSet r = SQLHandler.fetchPatientRow(patientTestID);
-////			if(r == null || !r.next()) throw new NoSuchPatientException();
-////			if(patientTestID != r.getInt(1)) throw new RPMError();
-////			PatientTestInfo ret = new PatientTestInfo(patientTestID);
-////			ret.load(r);
-////			return ret;
-//		}
-//		catch(NoSuchPatientException e)
-//		{
-//			throw e;
-//		}
-//		catch(Exception e)
-//		{
-//			Util.trace(e);
-//			throw new NoSuchPatientException();
-//		}
-//	}
+
 	
 	PatientTestInfo(ResultSet r) throws SQLException
 	{
